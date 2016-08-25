60611e78f4aa54619cc9bd3d3903b779bca0828e
anusha00zoa <anusha00zoa@gmail.com>
2016-08-25 11:31:01 -0500

    Creating UI

--- W02/ViewController.swift
+++ W02/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var totalTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,48 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func UndoSum(_ sender: AnyObject) {
+    }
+    
+    @IBAction func ClearAll(_ sender: AnyObject) {
+    }
 
+    @IBAction func AddOne(_ sender: AnyObject) {
+    }
 
+    @IBAction func AddTwo(_ sender: AnyObject) {
+    }
+    
+    @IBAction func AddThree(_ sender: AnyObject) {
+    }
+    
+    @IBAction func AddFour(_ sender: AnyObject) {
+    }
+    
+    @IBAction func AddFive(_ sender: AnyObject) {
+    }
+    
+    @IBAction func AddSix(_ sender: AnyObject) {
+    }
+    
+    @IBAction func AddSeven(_ sender: AnyObject) {
+    }
+    
+    @IBAction func AddEight(_ sender: AnyObject) {
+    }
+    
+    @IBAction func AddNine(_ sender: AnyObject) {
+    }
+    
+    @IBAction func AddTen(_ sender: AnyObject) {
+    }
+    
+    @IBAction func AddEleven(_ sender: AnyObject) {
+    }
+    
+    @IBAction func AddTwelve(_ sender: AnyObject) {
+    }
 }
 
